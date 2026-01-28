003c200144a1f3fd10d6eaa69c996bd2c1baccd2
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-28 12:20:58 +0530

    Introduction

---
+++ intro.py
@@ -0,0 +1,58 @@
+#these are printing statements
+# print(99)
+# print(100)
+# print(101)
+# print(90)
+#Print(30)
+
+
+'''Virat Kohli[a] (born 5 November 1988) is an Indian international cricketer and the former all-format captain of the Indian national cricket team.
+[3] He is a right-handed batter and occasional right-arm medium pace bowler. 
+Considered one of the greatest all-format batsmen in the history of cricket, he has been nicknamed the King,'''
+
+# a=10
+# print(a)
+
+# a1=20
+# print(a1)
+
+# Manager_age=55
+# print(Manager_age)
+
+# a=10
+# a=12
+# print(a)
+
+# a=10 
+# b=12.22
+# c="Hello"
+# d='Hello'
+
+# print(type(d)) #<class 'int'>
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# print("I'm from TN")
+# print('My fav line is "Do or die"')
+
+# print(4+5) # addition
+
+# print("Hello"+"Hi") # concat
+
+# print("Hello"+5) # error
+
+# name="Vijay"
+# age=51
+
+#His name is Vijay. He is 51 years old!
+
+# print("his name is " + name +". he is " + str(age) + " years old!")
+
+# a=input("Enter a value: ")
+# print(a)
+
+inp=input("Enter a number: ") 
+print(int(inp)+100)
