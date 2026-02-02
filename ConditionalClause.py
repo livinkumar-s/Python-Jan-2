a7774dd6468e6cf3fbfcbf066e6a3a86c337c4ea
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-02 11:37:54 +0530

    Conditional Clause

---
+++ ConditionalClause.py
@@ -0,0 +1,52 @@
+# print(90)
+# print(80)
+# print(70)
+
+# age=int(input("Enter Your Age: "))
+# if age>=18:
+#     print("Can vote!") #18,19,20...
+# else:
+#     print("Cannot vote") #17,16...
+
+# num=10
+
+# if num<3:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# elif num==3:
+#     print("From Elif 1 block")
+# elif num<3:
+#     print("From Elif 2 block")
+
+# print("Final Step!")
+
+# mark = int(input("Enter mark (0-100): "))
+# if mark >= 90 and mark <= 100:
+#     print("Grade: O")
+# elif mark >= 80 and mark <= 89:
+#     print("Grade: A+")
+# elif mark >= 70 and mark <= 79:
+#     print("Grade: A")
+# elif mark >= 60 and mark <= 69:
+#     print("Grade: B")
+# elif mark >= 50 and mark <= 59:
+#     print("Grade: C")
+# elif mark >= 0 and mark <= 49:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid Mark")
+
+
+# num=15
+
+# if num>10:
+#     if num>15:
+#         print("Very large")
+#     else:
+#         print("Large")
+# else:
+#     if num<5:
+#         print("Very Small")
+#     else:
+#         print("Small")
