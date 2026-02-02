d10ec1818d4828d958bc5ed76ff6e45a90cc8a70
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-02 12:20:42 +0530

    Loops - For, While

---
+++ Loops.py
@@ -0,0 +1,31 @@
+# i=0
+
+# while i>5:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     i+=1
+
+# no of iteration --> 
+# i--> 0
+
+
+# str1="Hello world"
+
+# # print(str1[6])
+# print(len(str1))
+
+# For Loop 
+
+# str1="Virat"
+
+# for i in str1:
+#     print(i)
+#     print("------------------")
+
+# i ---> 
+
+
+# for i in range(4,15,3):
+#     print(i)
+#     print("-------------")
