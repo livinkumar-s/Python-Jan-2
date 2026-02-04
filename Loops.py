9f45ad82d4fea3013247957d44597428d545fe22
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-04 12:23:36 +0530

    Nested Loops

--- Loops.py
+++ Loops.py
@@ -29,3 +29,85 @@
 # for i in range(4,15,3):
 #     print(i)
 #     print("-------------")
+
+# i=0
+
+# while i<100:
+#     if i<=25:
+#         break
+#     print(i)
+#     i+=1
+
+# i = 0
+# No of Iteration: 26
+
+# for i in range(1,101):
+#     print(i)
+#     if i==40:
+#         continue
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         continue
+#     print(i)
+#     i+=1
+
+# no of iteration: infinite, 1,2,3,4
+
+# For 
+
+# for i in range(1,101):
+#     print(i)
+
+# secret_number=33
+# NotFound=True
+
+# while NotFound:
+#     inp=int(input("Enter the number: "))
+#     if secret_number==inp:
+#         print("You Won!")
+#         NotFound=False
+#     else:
+#         print("Try again!")
+
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("i=",i,'j=',j)
+
+
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+# print("*****")
+
+# for i in range(1,6):
+#     print("* "*i)
+
+# print("Hello"*4)
+
+# print("Hello",end="")
+# print("Hi",end="")
+# print("World")
+
+# print("* * * * ")
+# print("*     * ")
+# print("*     * ")
+# print("* * * * ")
+
+inp=int(input("Enter the length: ")) #-->5//2--->2+1
+
+for i in range(1,inp+1):
+    for j in range(1,inp+1):
+        if j==1 or j==inp:
+            print("* ",end='')
+        elif i==j and i<=inp//2+1:
+            print("* ",end='')
+        elif j+i==inp+1 and i<=inp//2+1:
+            print("* ",end='')
+        else:
+            print("  ",end='')
+    print()
+
