4335bdf18da95c6ff51542a4f5a5141ecedb48b8
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-06 12:10:40 +0530

    Datastructures

---
+++ Datastructures.py
@@ -0,0 +1,87 @@
+# print(lis1[2])
+# lis1[3]=44
+# print(len(lis1))
+
+# print(lis1[-2])
+# lis1=[1,2,3,4,'one','two','three',3,3,3,3,3]
+
+# print(lis1[5:]) #5...11
+# print(lis1[3:7]) #3,4,5,6
+# print(lis1[::-1])
+
+
+# lis1.append(100)
+# lis1.append(101)
+# lis1.extend([1,2,3])
+
+# lis1.extend("Hello")
+# lis1.insert(2,33)
+# lis1.remove(23)
+# lis1.pop(2)
+# lis1.pop()
+
+# lis1=[23,324,213,5,234,3,23,24,25]
+
+# print(lis1.count(2))
+# print(lis1.index(231))
+# lis1.reverse()
+# lis1.sort()
+# lis1.clear()
+# lis1.sort()
+# lis1.reverse()
+# print(lis1)
+
+# Tuple
+
+# tup1=(2,3,4,5,6,7,8,6,7,8)
+
+# tup1[0]=0
+
+# person1=("Harsha",22,"hELLEN")
+# name,age,spouse=person1
+
+# print(age)
+
+
+# Set 
+
+s1={2,3,4,32,3,4,3,45,43}
+s2={43,23,54,54,46,65}
+# s1.add(90)
+# s1.update([7,8,9])
+# s1.clear()
+
+
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print(s1.difference(s2))
+# print(s2.difference(s1))
+
+# for i in s1:
+#     print(i)
+
+# Dictionary
+
+# dic1={
+#     "name":"Vijay",
+#     "age":53,
+#     "Role":"Police",
+#     "isMarried":True
+# }
+
+# for i in dic1:
+#     print(dic1[i])
+
+# dic1["age"]=52
+
+# dic1["latestMovie"]="GOAT"
+
+# print(dic1.keys())
+# print(dic1.items())
+# print(dic1.values())
+# dic1.pop("age")
+# print(dic1)
+
+lis1=[1,2,3,["four","five","six",["seven","eight","nine"]]]
+
+print(lis1[-1][-1][-1][-1])
