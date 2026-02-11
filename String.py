1c5c73c73499be6140e142560ee7a04071a8fbd2
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-11 11:37:37 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,48 @@
+# a="Hello"
+# b="World"
+
+# print(a+b)
+# print(a*4)
+
+
+# print("wo" in b)
+# print("Wo" not in b)
+
+# name="Balakrishna"
+# age=60
+
+# res="His name is "+name+". He is "+str(age)+" years old..!"
+# res=f"His name is {name}. He is {age} years old..!"
+# print(res)
+
+# name="Hendry"
+# # name+=" Pal"
+
+# name[2]="t"
+
+# print(name)
+
+
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.title())
+# print(str1.capitalize())
+
+str1="helloworldhello"
+# print(str1.index("y"))
+# print(str1.find("he"))
+
+# print(str1.isalpha())
+
+# print(str1.endswith("dl"))
+# print(str1.startswith("e"))
+# print(str1.replace("hello","hi"))
+
+str2="     I would love to play cricket       "
+lis1=['I', 'would', 'love', 'to', 'play', 'cricket']
+
+#[]
+
+# res=str2.split()
+# res="---".join(lis1)
+print(str2.strip())
